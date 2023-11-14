459f2002c406d9d480c133dfc23972684b53ac30
Afzal Masood <afzal.masood11@gmail.com>
2023-11-14 19:49:39 +0500

    Add error handling.

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,19 +1,23 @@
 const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
 
-const createUser = async (req, res) => {
-    const email = req.body.email;
-    const findUser = await User.findOne({email: email});
-    if(!findUser){
-        // Create new user
-        const newUser = await User.create(req.body);
-        res.json(newUser);
-    }else{
-        // User already exist
-        res.json({
-            msg: "User already exist",
-            success: false,
-        });
+const createUser = asyncHandler(
+    async (req, res) => {
+        const email = req.body.email;
+        const findUser = await User.findOne({email: email});
+        if(!findUser){
+            // Create new user
+            const newUser = await User.create(req.body);
+            res.json(newUser);
+        }else{
+            // User already exist
+            // res.json({
+            //     msg: "User already exist",
+            //     success: false,
+            // });
+            throw new Error('User Already Exists.');
+        }
     }
-}
+);
 
 module.exports = { createUser };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const dbConnect = require('./config/dbConnect');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 // });
 
 app.use('/api/user', authRouter);
+
+
+// Need to pass this middleware AFTER ALL THE routes
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running at Port: ${PORT}`);
