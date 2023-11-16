241efe346985b9f030f111ccfa8bbc56500bb97c
Afzal Masood <afzal.masood11@gmail.com>
2023-11-16 12:40:32 +0500

    Add JWT token and update user function.

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,3 +1,4 @@
+const { generateToken } = require('../config/jwtToken');
 const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler');
 
@@ -25,10 +26,40 @@ const loginUserCtrl = asyncHandler(
         const {email, password} = req.body;
         const findUser = await User.findOne({email});
         if(findUser && (await findUser.isPasswordMatched(password))){
-            res.json(findUser);
+            res.json({
+                _id: findUser?._id,
+                firstname: findUser?.firstname,
+                lastname: findUser?.lastname,
+                email: findUser?.email,
+                mobile: findUser?.mobile,
+                token: generateToken(findUser?._id)
+            });
         } else {
             throw new Error('Invalid credentials');
         }
     }
 );
-module.exports = { createUser, loginUserCtrl };
+
+const updateUserCtrl = asyncHandler(
+    async(req, res) => {
+        const { id } = req.params;
+        console.log(id);
+        try{
+            const updatedUser = await User.findByIdAndUpdate(id, {
+                firstname: req?.body?.firstname,
+                lastname: req?.body?.lastname,
+                mobile: req?.body?.mobile,
+                email: req?.body?.email,
+                },
+                {
+                    new: true
+                }
+            );
+            res.json(updatedUser);
+        }catch(error){
+            throw new Error(error);
+        }
+    }
+);
+
+module.exports = { createUser, loginUserCtrl, updateUserCtrl };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createUser, loginUserCtrl } = require('../controller/userCtrl');
+const { createUser, loginUserCtrl, updateUserCtrl } = require('../controller/userCtrl');
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
+router.put("/:id", updateUserCtrl);
 
 module.exports = router;
